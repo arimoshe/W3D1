342f7d7d051812efb62eb1a078871ba484a4606c
Ari Moshe <arimoshe@gmail.com>
2022-06-27 15:10:37 -0700

    working on my_zip

--- enumerables.rb
+++ enumerables.rb
@@ -41,17 +41,45 @@ class Array
 
     def my_all?(&prc)
         self.my_each do |ele|
-            return false if prc.call(ele)
+            return false if !prc.call(ele)
         end
 
         true
     end
 
+    def my_flatten
+
+        flatten = []
+        self.each do |ele|
+            if ele.is_a? (Array)
+                flatten += ele.my_flatten
+            else
+                flatten << ele
+            end
+        end
+        flatten
+    end
+    
+    def my_zip(*args)
+        new_arr=[]
+        (0...args.length+1).each do |i|
+            temp=[self[i]]
+            args.each do |arg|
+                temp << arg[i]
+            end
+            new_arr << temp
+        end
+        new_arr
+    end
 
 end
 
-a = [1, 2, 3]
-p a.my_any? { |num| num > 1 } # => true
-p a.my_any? { |num| num == 4 } # => false
-p a.my_all? { |num| num > 1 } # => false
-p a.my_all? { |num| num < 4 } # => true
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
